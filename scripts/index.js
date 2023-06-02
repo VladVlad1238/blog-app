93afc0b7f7c71bd89f825f81fc07b32976c36f70
Vladyslav Sikalo <vladislavmediol@gmail.com>
2023-06-02 17:28:21 +0200

    change design + hover

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,11 @@
 let posts = [];
 
 
-const TITLE_INPUT_LIMIT = 10;
-const TEXT_INPUT_LIMIT = 20;
+const TITLE_INPUT_LIMIT = 100;
+const TEXT_INPUT_LIMIT = 200;
+const RESET_TEXT_INPUT = 'You have 100 symbols';
+const RESET_TEXT = 'You have 200 symbols';
+const BLOG_CONTENT_LIST_TEXT = '<div class="js-content-list blog__content-lists">Still empty....</div>';
 
 
 const titleInputNode = document.querySelector('.js-input-title');
@@ -13,51 +16,32 @@ const textValidationNode = document.querySelector('.js-text-validation');
 const publishButtonNode = document.querySelector('.js-publish-btn');
 const publishButtonDisabled = document.querySelector('.js-button__disabled');
 const deleteBlogButtonNode = document.querySelector('.js-delete-blog-btn');
-
-
-
-
-
-
-
-const validation = () => {
-  const titleLen = titleInputNode.value.length;
-  const textLen = textInputNode.value.length;
-  
-  const titleMaxLen = TITLE_INPUT_LIMIT - titleInputNode.value.length; 
-  const textMaxLen = TEXT_INPUT_LIMIT - textInputNode.value.length; 
-
-  if(titleLen <= TITLE_INPUT_LIMIT) {
-    inputValidationNode.innerText = `You have ${titleMaxLen} symbols`;
-  } else {
-    inputValidationNode.innerText = `Title more than ${TITLE_INPUT_LIMIT} symbols`;
-  }
-
-  if(textLen <= TEXT_INPUT_LIMIT) {
-    textValidationNode.innerText = `You have ${textMaxLen} symbols`;
-  } else {
-    textValidationNode.innerText = `Text more than ${TEXT_INPUT_LIMIT} symbols`;
-  }
-};
-inputValidationNode.addEventListener('input', validation());
-textValidationNode.addEventListener('input', validation());
+const blogContentListNode = document.querySelector('.js-content-list');
 
 
 const getPostFromUser = () => {
   const title = titleInputNode.value;
   const text = textInputNode.value;
-
+  const options = {hour: 'numeric', minute: 'numeric'};
+  const date = new Date().toLocaleDateString();
+  const time = new Date().toLocaleTimeString([], options);
+  
   return {
     title: title,
     text: text,
+    date: date,
+    time: time,
   }
 };
 
-const addPosts = ({title, text}) => {
+const addPosts = ({title, text, date, time}) => {
   posts.push({
     title,
     text,
+    date,
+    time,
   })
+  return
 };
 
 const getPosts = () => {
@@ -72,6 +56,7 @@ const renderPost = () => {
   posts.forEach((post) => {
      postsHTML += `
       <div class='post'>
+        <p class='post__date-time'>${post.date} ${post.time}</p>
         <p class='post__title'>${post.title}</p>
         <p class='post__text'>${post.text}</p>
       </div>`
@@ -82,12 +67,40 @@ const renderPost = () => {
 const clearInput = () => {
   titleInputNode.value = '';
   textInputNode.value = '';
+  inputValidationNode.innerText = RESET_TEXT_INPUT;
+  textValidationNode.innerText = RESET_TEXT;
 };
 
 const deleteBlog = () => {
   posts = [];
+  blogContentListNode.innerHTML = `${BLOG_CONTENT_LIST_TEXT}`;
   renderPost();
 }
+
+const validation = () => {
+  const titleLen = titleInputNode.value.length;
+  const textLen = textInputNode.value.length;
+  
+  const titleMaxLen = TITLE_INPUT_LIMIT - titleInputNode.value.length; 
+  const textMaxLen = TEXT_INPUT_LIMIT - textInputNode.value.length; 
+
+  if(titleLen <= TITLE_INPUT_LIMIT) {
+    inputValidationNode.innerText = `You have ${titleMaxLen} symbols`;
+    inputValidationNode.classList.remove('color_red');
+  } else {
+    inputValidationNode.innerText = `Title more than ${TITLE_INPUT_LIMIT} symbols`;
+    inputValidationNode.classList.add('color_red');
+  }
+
+  if(textLen <= TEXT_INPUT_LIMIT) {
+    textValidationNode.innerText = `You have ${textMaxLen} symbols`;
+    textValidationNode.classList.remove('color_red');
+  } else {
+    textValidationNode.innerText = `Text more than ${TEXT_INPUT_LIMIT} symbols`;
+    textValidationNode.classList.add('color_red');
+  }
+};
+
 
 const buttonDisabled = () => {
   if(titleInputNode.value.length > 0 &&
@@ -95,20 +108,14 @@ const buttonDisabled = () => {
      publishButtonNode.removeAttribute('disabled');
     } else {
      publishButtonNode.setAttribute('disabled', 'disabled');
+     publishButtonNode.classList.add('btn-disabled');
     }
 
-  if(titleInputNode.value.length > 10 &&
-    textInputNode.value.length > 20) {
+  if(titleInputNode.value.length > 100 &&
+    textInputNode.value.length > 200) {
       publishButtonNode.setAttribute('disabled', 'disabled');
     }
 };
-
-
-
-
-
-
-
 
 const publishButtonHandler = () => {
   const postFromUser = getPostFromUser();
@@ -125,11 +132,11 @@ const publishButtonHandler = () => {
 
 const deleteBlogButtonHandler = () => {
   deleteBlog();
-
 };
-
 
 
 publishButtonNode.addEventListener('click', publishButtonHandler);
 deleteBlogButtonNode.addEventListener('click', deleteBlogButtonHandler);
 publishButtonDisabled.addEventListener('input', buttonDisabled);
+inputValidationNode.addEventListener('input', validation());
+textValidationNode.addEventListener('input', validation());
